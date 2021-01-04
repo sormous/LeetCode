3d958488fb9a4232844ff4a0538e47cde4480685
sormous <504585322@qq.com>
2021-01-04 09:37:56 +0800

    commit

--- 509.斐波那契数.java
+++ 509.斐波那契数.java
@@ -5,13 +5,29 @@
  */
 
 // @lc code=start
+/**
+ * 可以利用一个数组来存储已经计算过的fibo数，避免重复计算导致效率低下
+ * 以及可以利用动态规划的思想，初始化一个dp数组，来完成fibo数的推导
+ */
 class Solution {
     public int fib(int N) {
         if(N==0) return 0;
-        if(N==1) return 1;
-        return fib(N-1) + fib(N-2);
+        int[] memo = new int[N+1];
+        return helper(memo, N);
+        /**
+         * dp[1] = dp[2] = 1
+         * dp[i] = dp[i-1] + dp[i-2]
+         */
     }
 
+    private int helper(int[] memo, int N){
+        if(N==1 || N==2){
+            return 1;
+        }
+        if(memo[N] != 0) return memo[N];
+        memo[N] = helper(memo, N-1) + helper(memo, N-2);
+        return memo[N];
+    }
 }
 // @lc code=end
 
