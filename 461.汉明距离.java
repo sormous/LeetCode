b908bd38b6a4706589b3da80cff96ba2c6e38269
sormous <504585322@qq.com>
2021-05-27 10:21:00 +0800

    commit

--- 461.汉明距离.java
+++ 461.汉明距离.java
@@ -12,12 +12,14 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int z = x ^ y;
-        int sum = 0;
-        while(z != 0){
-            sum += z&1;
-            z = z>>1;
-        }
-        return sum;
+        return  Integer.bitCount(z);
+
+        // int sum = 0;
+        // while(z != 0){
+        //     sum += z&1;
+        //     z = z>>1;
+        // }
+        // return sum;
     }
 }
 // @lc code=end
